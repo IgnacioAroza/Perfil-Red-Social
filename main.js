066d6beb731a3fd5014cd32317a07c8555e6e5ee
Ignacio Aroza <ignacioaroza.ia@gmail.com>
2023-04-28 14:39:24 -0300

    Trabajo Practico terminado

---
+++ main.js
@@ -0,0 +1,69 @@
+const btnSeguir = document.getElementById('seguir');
+const btnMeGusta = document.getElementById('meGusta');
+let botonPresionado = false;
+
+btnSeguir.addEventListener('click', function(){
+    if(btnSeguir.innerHTML === "Seguir"){
+        btnSeguir.innerHTML = "Dejar de seguir";
+    }else {
+        btnSeguir.innerHTML = "Seguir";
+    }
+});
+
+function sumarLike(){
+    const meGustas = document.querySelector('.cantLikes');
+
+    if(botonPresionado){
+        meGustas.innerHTML--;
+    }else{
+        meGustas.innerText++;
+    }
+    botonPresionado = !botonPresionado;
+};
+
+const inpComentarios = document.querySelector('.inp-comentarios');
+
+inpComentarios.addEventListener('click', (evento) => {
+    evento.preventDefault();
+
+    const inpUsuario = document.getElementById('inpUsuario');
+    const inpComentario = document.getElementById('inp-coment');
+    const usuario = inpUsuario.value.trim();
+    const comentario = inpComentario.value.trim();
+
+    if(!usuario){
+        inpUsuario.innerText = "Por favor, ingresa tu usuario";
+        return;
+    }
+
+    if(!comentario){
+        inpComentario.innerText = "Por favor, ingresa un comentario";
+        return;
+    }
+
+    agregarComentario(usuario, comentario);
+});
+
+function agregarComentario(usuario, comentario){
+    const contenedorComentarios = document.querySelector('.comentarios');
+    const nuevoComentario = document.createElement('p');
+    nuevoComentario.classList.add('comentario');
+    nuevoComentario.innerHTML = `<strong class="nombre">${usuario}</strong> ${comentario}`;
+    const botonBorrar = document.createElement('button');
+    botonBorrar.classList.add('btn-borrar');
+    botonBorrar.innerText = "Eliminar";
+    nuevoComentario.appendChild(botonBorrar);
+    contenedorComentarios.appendChild(nuevoComentario);
+    limpiarFormulario();
+
+    botonBorrar.addEventListener('click', () => {
+        contenedorComentarios.removeChild(nuevoComentario);
+    })
+}
+
+function limpiarFormulario(){
+    const inpUsuario = document.getElementById('inpUsuario');
+    const inpComentario = document.getElementById('inp-coment');
+    inpUsuario.value = "";
+    inpComentario.value = "";
+}
